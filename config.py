61494f2ac0b1efefd1047ea58ed243429b26c23b
Kamil Monicz <kamil@monicz.pl>
2023-07-04 19:41:47 +0200

    Version bump 1.1

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ if DRY_RUN:
 else:
     print('🔴 PRODUCTION MODE 🔴')
 
-VERSION = '1.0'
+VERSION = '1.1'
 CREATED_BY = f'osm-warszawa-restauracje {VERSION}'
 WEBSITE = 'https://github.com/Zaczero/osm-warszawa-restauracje'
 USER_AGENT = f'osm-warszawa-restauracje/{VERSION} (+{WEBSITE})'
